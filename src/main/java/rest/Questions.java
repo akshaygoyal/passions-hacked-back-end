77aae14c0a483708fa32856d558112b8cb5594f1
akshaygoyal <akshay.goyal@inmobi.com>
2016-10-14 18:24:07 +0200

    static content

--- src/main/java/rest/Questions.java
+++ src/main/java/rest/Questions.java
@@ -1,5 +1,10 @@
 package rest;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.google.gson.Gson;
 import data.Card;
 import data.Deck;
@@ -10,22 +15,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Questions {
 
+  private List<Card> cards;
+
   @RequestMapping("/questions/{state}")
   public String getNextSetOfQuestions(@PathVariable("state") String state) {
 
     Deck deck = new Deck();
 
-    for(int i = 0 ; i < 3; i++) {
-      Card card = new Card();
-      card.setId(i);
-      card.setDesc("");
-      card.setId(i);
-    }
-
-
+    List<Card> cards = getCards();
+    deck.setCards(cards);
     String json = (new Gson()).toJson(deck);
     return json;
   }
 
 
+  public List<Card> getCards() {
+    ClassLoader classLoader = getClass().getClassLoader();
+    InputStream stream = classLoader.getResourceAsStream("themes.txt");
+
+    int i = 0;
+    List<Card> cards = new ArrayList<Card>();
+    Scanner scanner = new Scanner(stream);
+
+    while (scanner.hasNextLine()) {
+      String[] line = scanner.nextLine().split(",");
+      Card card = new Card();
+      card.setId(i);
+      card.setDesc(line[0]);
+      card.setImage_url(line[1]);
+      cards.add(card);
+      i++;
+    }
+    scanner.close();
+    return cards;
+  }
 }
