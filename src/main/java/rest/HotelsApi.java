dd4afa451415b3243481bf2bacf659eaf4839199
akshaygoyal <akshay.goyal@inmobi.com>
2016-10-15 15:26:39 +0200

    experiment

--- src/main/java/rest/HotelsApi.java
+++ src/main/java/rest/HotelsApi.java
@@ -1,6 +1,8 @@
 package rest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -23,36 +25,44 @@ public class HotelsApi {
   @RequestMapping(path = "/hotels", method = RequestMethod.POST)
   public String getListOfHotels(@RequestBody final String state) {
 
-//    URL getHotelsUrl = null;
-//    JsonObject jo = null;
-//    String data = "";
-//    try {
-//
-//      getHotelsUrl = new URL(baseUrl + "getHotels?city_ids=-3875419");
-//      HttpURLConnection conn = (HttpURLConnection) getHotelsUrl.openConnection();
-//      conn.setRequestMethod("GET");
-//      conn.setRequestProperty("Accept", "application/json");
-//      conn.setRequestProperty("Authorization", "Basic aGFja2VyMjQwOjZQSmZ5UUZMbjQ=");
-////      data = (String) conn.res;
-//
-//      JsonParser parser = new JsonParser();
-//      jo = (JsonObject) parser.parse(data);
-//      System.out.println(jo.getAsJsonArray().toString());
-//
-//      System.out.println(jo.get("hotel_id"));
-//
-//    } catch (MalformedURLException e) {
-//      e.printStackTrace();
-//    } catch (ProtocolException e) {
-//      e.printStackTrace();
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-//
-//
-////    Hotel hotel = new Hotel();
+    URL getHotelsUrl = null;
+    JsonObject jo = null;
+    String data = "";
+    try {
 
-    String json = (new Gson()).toJson(MockUtil.getHotels());
-    return json;
+      getHotelsUrl = new URL(baseUrl + "getHotels?city_ids=-2140479");
+      HttpURLConnection conn = (HttpURLConnection) getHotelsUrl.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Accept", "application/json");
+      conn.setRequestProperty("Authorization", "Basic aGFja2VyMjQwOjZQSmZ5UUZMbjQ=");
+
+      StringBuilder result = new StringBuilder();
+      if(conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+        String line;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        while ((line = reader.readLine()) != null) {
+          result.append(line);
+        }
+      }
+
+      JsonParser parser = new JsonParser();
+      jo = (JsonObject) parser.parse(result.toString());
+      System.out.println(jo.getAsJsonArray().toString());
+
+      System.out.println(jo.get("hotel_id"));
+
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (ProtocolException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+
+//    String json = (new Gson()).toJson(MockUtil.getHotels());
+    return jo.getAsJsonArray().toString();
+
+
   }
 }
